f0e5fc0c36e1ec009f7c90cc0502fffca768a95d
greicyitakura <66883987+greicyitakura@users.noreply.github.com>
2021-11-09 18:59:42 -0300

    Add files via upload

---
+++ backend/backend/src/main/java/com/greicy/dsvendas/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.greicy.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.greicy.dsvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long>{
+
+}
---
+++ backend/backend/src/main/java/com/greicy/dsvendas/service/SaleService.java
@@ -0,0 +1,42 @@
+package com.greicy.dsvendas.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.greicy.dsvendas.dto.SaleDTO;
+import com.greicy.dsvendas.dto.SaleSuccessDTO;
+import com.greicy.dsvendas.dto.SaleSumDTO;
+import com.greicy.dsvendas.entities.Sale;
+import com.greicy.dsvendas.repositories.SaleRepository;
+import com.greicy.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SaleService {
+	
+	@Autowired
+	private SaleRepository repository;
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	@Transactional(readOnly = true)
+	public Page<SaleDTO> findAll(Pageable pageable) {
+		sellerRepository.findAll();
+		Page<Sale> result = repository.findAll(pageable);	
+		return result.map(x -> new SaleDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller(){
+	return repository.amountGroupedBySeller();
+	}
+
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> sucessGroupedBySeller(){
+	return repository.sucessGroupedBySeller();
+	}
+}
