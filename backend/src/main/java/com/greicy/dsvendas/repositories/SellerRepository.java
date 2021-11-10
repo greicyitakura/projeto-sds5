7183781a7803261ef648051edd45bddfeb17832f
greicyitakura <66883987+greicyitakura@users.noreply.github.com>
2021-11-10 11:58:05 -0300

    Delete backend directory

--- backend/src/main/java/com/greicy/dsvendas/repositories/SellerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.greicy.dsvendas.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.greicy.dsvendas.entities.Seller;
-
-public interface SellerRepository extends JpaRepository<Seller, Long>{
-
-}
